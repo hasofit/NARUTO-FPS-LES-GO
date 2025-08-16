b52825e95bed08529330ebb0faafdb72741b9bac
hasofit <notsus0205@gmail.com>
2025-08-17 02:30:55 +0300

    enemy strafing for coperniq aim trainer

--- scripts/test_dummy.gd
+++ scripts/test_dummy.gd
@@ -1,11 +1,34 @@
 extends RigidBody3D
 
 @onready var player: CharacterBody3D = $"../../Player"
+@onready var change: Timer = $Change
 
 @export var health = 10
+@export var ai = false
+@export var SPEED = 300
+@export var turn_time = 1
+
+var left = false
+
+func _ready() -> void:
+	change.start(turn_time)
 
 func _physics_process(delta: float) -> void:
 	if health < 1:
 		player.enemies_count -= 1
 		player.enemy_count.text = str(player.enemies_count)
 		queue_free()
+	if ai:
+		if left:
+			global_position = global_position + Vector3.LEFT * SPEED * delta
+		elif !left:
+			global_position = global_position + Vector3.RIGHT * SPEED * delta
+		lock_rotation = true
+	else:
+		lock_rotation = false
+
+func _on_change_timeout() -> void:
+	if left:
+		left = false
+	else:
+		left = true
