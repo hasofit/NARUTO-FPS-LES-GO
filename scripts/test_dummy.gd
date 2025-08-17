0f18956ce1d267df2533d414b603af1919968239
hasofit <notsus0205@gmail.com>
2025-08-17 04:03:40 +0300

    FIXED IT!

--- scripts/test_dummy.gd
+++ scripts/test_dummy.gd
@@ -2,33 +2,30 @@ extends RigidBody3D
 
 @onready var player: CharacterBody3D = $"../../Player"
 @onready var change: Timer = $Change
+@onready var shooting_point: Marker3D = $shooting_point
 
+@export var world : Node3D
+@export var kunai : PackedScene
 @export var health = 10
 @export var ai = false
+@export var shooting = false
 @export var SPEED = 300
-@export var turn_time = 1
+@export var turn_time : float = 1
 
 var left = false
 
-func _ready() -> void:
-	change.start(turn_time)
-
-func _physics_process(delta: float) -> void:
-	if health < 1:
-		player.enemies_count -= 1
-		player.enemy_count.text = str(player.enemies_count)
-		queue_free()
-	if ai:
-		if left:
-			global_position += -transform.basis.x * SPEED * delta
-		elif !left:
-			global_position += transform.basis.x * SPEED * delta
-		lock_rotation = true
-	else:
-		lock_rotation = false
-
 func _on_change_timeout() -> void:
+	if shooting:
+		shoot()
+	
 	if left:
 		left = false
 	else:
 		left = true
+
+func shoot():
+	var new_kunai = kunai.instantiate()
+	world.add_child(new_kunai)
+	new_kunai.global_transform = shooting_point.global_transform
+	
+	new_kunai.linear_velocity = new_kunai.global_transform.basis.z * 30.0
